672850db193031633772809d3ebc1fef4799d61c
Mehul <cs20077@glbitm.ac.in>
2022-10-31 17:28:49 +0530

    Updates

---
+++ precache-manifest.68063998bd40725cfeda07b56d15139e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c322b84d0f08a24073e",
+    "url": "/react-project-1/static/css/main.a0cb054d.chunk.css"
+  },
+  {
+    "revision": "3c322b84d0f08a24073e",
+    "url": "/react-project-1/static/js/main.3c322b84.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/react-project-1/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "2cf244f204f9a10aad07",
+    "url": "/react-project-1/static/js/2.2cf244f2.chunk.js"
+  },
+  {
+    "revision": "37e41d9addb2195fe305",
+    "url": "/react-project-1/static/js/runtime~main.37e41d9a.js"
+  },
+  {
+    "revision": "aa15d11e9bf87abe5b412a7000dda2a2",
+    "url": "/react-project-1/index.html"
+  }
+];
